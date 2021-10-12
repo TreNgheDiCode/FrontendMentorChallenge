63251228cddd9bc978995876c318fe8c696f245a
TreNgheDiCode <88233797+TreNgheDiCode@users.noreply.github.com>
2021-10-13 02:40:37 +0700

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector("#menu-btn");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeEle= document.querySelectorAll(".has-fade");
+
+menuBtn.addEventListener("click", function() {
+    console.log("clicked!");
+    if (header.classList.contains("open")) {
+        body.classList.remove("noscroll");
+        header.classList.remove("open"); // Close menu
+        fadeEle.forEach(function(element) {
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+        });
+        
+    } else {
+        body.classList.add("noscroll");
+        header.classList.add("open"); // Open menu
+        fadeEle.forEach(function(element) {
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+        });
+    }
+});
