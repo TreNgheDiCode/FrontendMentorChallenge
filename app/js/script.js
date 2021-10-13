2970035c48df23220299a20edcde9179fb25b426
TreNgheDiCode <88233797+TreNgheDiCode@users.noreply.github.com>
2021-10-13 17:01:28 +0700

    Add files via upload

--- app/js/script.js
+++ app/js/script.js
@@ -3,12 +3,10 @@ const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeEle= document.querySelectorAll(".has-fade");
-const hero = document.querySelector(".hero");
 
 menuBtn.addEventListener("click", function() {
     if (header.classList.contains("open")) {
         body.classList.remove("noscroll");
-        hero.classList.remove("inActive")
         header.classList.remove("open"); // Close menu
         fadeEle.forEach(function(element) {
             element.classList.remove("fade-in");
@@ -17,7 +15,6 @@ menuBtn.addEventListener("click", function() {
         
     } else {
         body.classList.add("noscroll");
-        hero.classList.add("inActive")
         header.classList.add("open"); // Open menu
         fadeEle.forEach(function(element) {
             element.classList.remove("fade-out");
